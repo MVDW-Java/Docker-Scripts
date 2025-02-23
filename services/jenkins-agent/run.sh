34fea7dbf1c5d4fc251841088dec7273fd357e64
MVDW <maxvdwolf@gmail.com>
2025-02-23 17:38:57 +0100

    Update run.sh

--- services/jenkins-agent/run.sh
+++ services/jenkins-agent/run.sh
@@ -14,7 +14,7 @@ DockerBackup -n jenkins-agent
 
 # Generate SSH keys for Jenkins agent if they don't exist
 JENKINS_AGENT_KEY=$(DockerSecrets -n jenkins-agent-key -t ssh-key)
-JENKINS_AGENT_PUBLIC_KEY=$(cat ../../secrets/jenkins-agent-key.pub)
+JENKINS_AGENT_PUBLIC_KEY=$(cat ../../secrets/jenkins-agent-key.pub | sed 's/"/\"/g')
 
 DockerRun \
   -i "jenkins-agent" \
@@ -22,7 +22,7 @@ DockerRun \
   --network "jenkins" \
   -v "jenkins-agent-data:/home/jenkins" \
   -v "/var/run/docker.sock:/var/run/docker.sock" \
-  -e "JENKINS_AGENT_SSH_PUBKEY=${JENKINS_AGENT_PUBLIC_KEY}" \
+  -e "JENKINS_AGENT_SSH_PUBKEY=\"${JENKINS_AGENT_PUBLIC_KEY}\"" \
   --group-add "$(getent group docker | cut -d: -f3)" \
   -d
 
