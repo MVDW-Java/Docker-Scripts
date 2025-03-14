b1900aadcf24a5fad131a59a0994478fd0e816a1
MVDW <maxvdwolf@gmail.com>
2025-03-14 11:54:53 +0100

    update jenkins-agent scripts

--- services/jenkins-agent/run.sh
+++ services/jenkins-agent/run.sh
@@ -1,3 +1,5 @@
+# Docker-Scripts/services/jenkins-agent/run.sh
+
 # include required scripts
 source ../../core/build.sh
 source ../../core/backup.sh
@@ -12,9 +14,14 @@ DockerBuild jenkins-agent $(pwd)/data/Dockerfile
 
 DockerBackup -n jenkins-agent
 
+# Generate SSH host keys if they don't exist
+DockerSecrets -n jenkins-agent-ssh-host-rsa -t ssh-key --key-type rsa
+DockerSecrets -n jenkins-agent-ssh-host-ecdsa -t ssh-key --key-type ecdsa
+DockerSecrets -n jenkins-agent-ssh-host-ed25519 -t ssh-key --key-type ed25519
+
 # Generate SSH keys for Jenkins agent if they don't exist
 JENKINS_AGENT_KEY=$(DockerSecrets -n jenkins-agent-key -t ssh-key)
-JENKINS_AGENT_PUBLIC_KEY=$(cat ../../secrets/jenkins-agent-key.pub | sed 's/"/\"/g')
+JENKINS_AGENT_PUBLIC_KEY=$(cat ../../secrets/jenkins-agent-key.pub)
 
 DockerRun \
   -i "jenkins-agent" \
@@ -22,8 +29,16 @@ DockerRun \
   --network "jenkins" \
   -v "jenkins-agent-data:/home/jenkins" \
   -v "/var/run/docker.sock:/var/run/docker.sock" \
-  -e "JENKINS_AGENT_SSH_PUBKEY=\"${JENKINS_AGENT_PUBLIC_KEY}\"" \
+  -v "$(pwd)/../../secrets/jenkins-agent-ssh-host-rsa:/etc/ssh/ssh_host_rsa_key" \
+  -v "$(pwd)/../../secrets/jenkins-agent-ssh-host-rsa.pub:/etc/ssh/ssh_host_rsa_key.pub" \
+  -v "$(pwd)/../../secrets/jenkins-agent-ssh-host-ecdsa:/etc/ssh/ssh_host_ecdsa_key" \
+  -v "$(pwd)/../../secrets/jenkins-agent-ssh-host-ecdsa.pub:/etc/ssh/ssh_host_ecdsa_key.pub" \
+  -v "$(pwd)/../../secrets/jenkins-agent-ssh-host-ed25519:/etc/ssh/ssh_host_ed25519_key" \
+  -v "$(pwd)/../../secrets/jenkins-agent-ssh-host-ed25519.pub:/etc/ssh/ssh_host_ed25519_key.pub" \
+  -e "JENKINS_AGENT_SSH_PUBKEY=$JENKINS_AGENT_PUBLIC_KEY" \
+  -e "SKIP_SSH_KEYGEN=true" \
   --group-add "$(getent group docker | cut -d: -f3)" \
+  --restart "unless-stopped" \
   -d
 
 echo "Jenkins agent SSH private key (save this for Jenkins credentials):"
